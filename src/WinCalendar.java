cc657fee0bc92e3e1d4e6153c6dea61cd0043cb7
gmlrnjsdla <dlagmlrnjs@gmail.com>
2022-11-07 16:42:35 +0900

    DB연동 및 달력 제작

---
+++ src/WinCalendar.java
@@ -0,0 +1,392 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Calendar;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class WinCalendar extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private Calendar now;
+	private JPanel panelCalendar;
+	private JComboBox cbMonth;
+	private JComboBox cbYear;
+	private JPanel panel_1;
+	private JLabel lblresult;
+	private JButton btnNewButton;
+	private JButton btnNewButton_1;
+	private JButton btnNewButton_2;
+	private JButton btnNewButton_3;
+	private JButton btnNewButton_4;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			WinCalendar dialog = new WinCalendar();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+
+	/**
+	 * Create the dialog.
+	 */
+	public WinCalendar() {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowOpened(WindowEvent e) {
+				Component compList[] = panelCalendar.getComponents();
+				for(Component c : compList) {
+					if(c instanceof JButton) {
+						panelCalendar.remove(c);
+					}
+					panelCalendar.revalidate();
+					panelCalendar.repaint();
+				}
+				
+				String day[] = {"일","월","화","수","목","금","토"};
+				int Months[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+				int year = (int)cbYear.getSelectedItem();
+				int month = (int)cbMonth.getSelectedItem();
+				for(int i=0; i<7; i++) {
+					JButton btn = new JButton(day[i]);
+					btn.setBackground(Color.pink);
+					panelCalendar.add(btn);
+				}
+				
+				// 1922년 1월 1일 일요일 시작
+				int index = 0;
+				int sum = 0;
+				
+				for(int i =1922; i<year; i++) {
+					if(i%4==0 && i%100 !=0 || i%400==0) {
+						sum = sum + 366;
+					}
+					else {
+						sum = sum + 365;
+					}
+				}
+				
+				int lastDay =0;
+				
+				for(int i = 0; i<month-1; i++) {
+					if(i ==1 && (year % 4 ==0 && year%100!=0 || year%400==0)) {
+						sum = sum + ++Months[i];
+						
+					}
+					else {
+						sum = sum + Months[i];
+					}
+					
+				}
+				if(month == 2 && (year % 4 ==0 && year%100!=0 || year%400==0)) {
+					lastDay = ++Months[month-1];
+				}
+				else {
+					lastDay = Months[month-1];
+				}
+				
+				index = (index+sum)%7;
+				
+				for(int i =1 ; i<=index; i++) {
+					JButton btn = new JButton("");
+					panelCalendar.add(btn);
+					btn.setVisible(false);
+				}
+				
+				for(int i=1; i<=lastDay; i++) {
+					JButton btn = new JButton(i+"일");
+					
+					
+					panelCalendar.add(btn);
+				}
+				
+				panelCalendar.revalidate();
+			}
+		});
+		setTitle("달력");
+		setBounds(100, 100, 514, 385);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(new BorderLayout(0, 0));
+		{
+			JPanel panel = new JPanel();
+			contentPanel.add(panel, BorderLayout.NORTH);
+			{
+				cbYear = new JComboBox();
+				
+				for(int i = 1922; i<=2100; i++) {
+					cbYear.addItem(i);
+				}
+				now = Calendar.getInstance();
+				{
+					btnNewButton_4 = new JButton("<<");
+					btnNewButton_4.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent e) {
+							int year = cbYear.getSelectedIndex();
+							year --;
+							if(year<0) {
+								year = cbYear.getItemCount();
+								
+							}
+							cbYear.setSelectedIndex(year);
+						}
+					});
+					panel.add(btnNewButton_4);
+					
+				}
+				{
+					btnNewButton_3 = new JButton("<");
+					btnNewButton_3.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent e) {
+							int month = cbMonth.getSelectedIndex();
+							month --;
+							if(month<0) {
+								month = 11;
+								
+							}
+							cbMonth.setSelectedIndex(month);
+							
+						}
+					});
+					panel.add(btnNewButton_3);
+				}
+				cbYear.setSelectedItem(now.get(Calendar.YEAR));
+				panel.add(cbYear);
+				cbYear.addActionListener(new ActionListener() {
+					
+					
+					
+					public void actionPerformed(ActionEvent e) {
+						Component compList[] = panelCalendar.getComponents();
+						for(Component c : compList) {
+							if(c instanceof JButton) {
+								panelCalendar.remove(c);
+							}
+							panelCalendar.revalidate();
+							panelCalendar.repaint();
+						}
+						
+						String day[] = {"일","월","화","수","목","금","토"};
+						int Months[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+						int year = (int)cbYear.getSelectedItem();
+						int month = (int)cbMonth.getSelectedItem();
+						for(int i=0; i<7; i++) {
+							JButton btn = new JButton(day[i]);
+							btn.setBackground(Color.pink);
+							panelCalendar.add(btn);
+						}
+						
+						// 1922년 1월 1일 일요일 시작
+						int index = 0;
+						int sum = 0;
+						
+						for(int i =1922; i<year; i++) {
+							if(i%4==0 && i%100 !=0 || i%400==0) {
+								sum = sum + 366;
+							}
+							else {
+								sum = sum + 365;
+							}
+						}
+						
+						int lastDay =0;
+						
+						for(int i = 0; i<month-1; i++) {
+							if(i ==1 && (year % 4 ==0 && year%100!=0 || year%400==0)) {
+								sum = sum + ++Months[i];
+								
+							}
+							else {
+								sum = sum + Months[i];
+							}
+							
+						}
+						if(month == 2 && (year % 4 ==0 && year%100!=0 || year%400==0)) {
+							lastDay = ++Months[month-1];
+						}
+						else {
+							lastDay = Months[month-1];
+						}
+						
+						index = (index+sum)%7;
+						
+						for(int i =1 ; i<=index; i++) {
+							JButton btn = new JButton("");
+							panelCalendar.add(btn);
+							btn.setVisible(false);
+						}
+						
+						for(int i=1; i<=lastDay; i++) {
+							JButton btn = new JButton(i+"일");
+							
+							
+							panelCalendar.add(btn);
+						}
+						
+						panelCalendar.revalidate();
+					}
+				});
+			}
+			{
+				cbMonth = new JComboBox();
+				
+				
+				for(int i = 1; i<=12; i++) {
+					cbMonth.addItem(i);
+				}
+				cbMonth.setSelectedItem(now.get(Calendar.MONTH)+1);
+				panel.add(cbMonth);
+				
+				cbMonth.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						
+						
+						
+						Component compList[] = panelCalendar.getComponents();
+						for(Component c : compList) {
+							if(c instanceof JButton) {
+								panelCalendar.remove(c);
+							}
+							panelCalendar.revalidate();
+							panelCalendar.repaint();
+						}
+						
+						String day[] = {"일","월","화","수","목","금","토"};
+						int Months[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+						int year = (int)cbYear.getSelectedItem();
+						int month = (int)cbMonth.getSelectedItem();
+						for(int i=0; i<7; i++) {
+							JButton btn = new JButton(day[i]);
+							btn.setBackground(Color.pink);
+							panelCalendar.add(btn);
+						}
+						
+						// 1922년 1월 1일 일요일 시작
+						int index = 0;
+						int sum = 0;
+						
+						for(int i =1922; i<year; i++) {
+							if(i%4==0 && i%100 !=0 || i%400==0) {
+								sum = sum + 366;
+							}
+							else {
+								sum = sum + 365;
+							}
+						}
+						
+						int lastDay =0;
+						
+						for(int i = 0; i<month-1; i++) {
+							if(i ==1 && (year % 4 ==0 && year%100!=0 || year%400==0)) {
+								sum = sum + ++Months[i];
+								
+							}
+							else {
+								sum = sum + Months[i];
+							}
+							
+						}
+						if(month == 2 && (year % 4 ==0 && year%100!=0 || year%400==0)) {
+							lastDay = ++Months[month-1];
+						}
+						else {
+							lastDay = Months[month-1];
+						}
+						
+						index = (index+sum)%7;
+						
+						for(int i =1 ; i<=index; i++) {
+							JButton btn = new JButton("");
+							panelCalendar.add(btn);
+							btn.setVisible(false);
+						}
+						
+						for(int i=1; i<=lastDay; i++) {
+							JButton btn = new JButton(i+"일");
+							
+							
+							panelCalendar.add(btn);
+						}
+						
+						panelCalendar.revalidate();
+					}
+				});
+			}
+			{
+				btnNewButton_1 = new JButton(">");
+				btnNewButton_1.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						int month = cbMonth.getSelectedIndex();
+						month ++;
+						if(month>=12) {
+							month = 0;
+							
+						}
+						cbMonth.setSelectedIndex(month);
+					}
+				});
+				panel.add(btnNewButton_1);
+			}
+			{
+				btnNewButton_2 = new JButton(">>");
+				btnNewButton_2.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						int year = cbYear.getSelectedIndex();
+						year ++;
+						if(year>=cbYear.getItemCount()) {
+							year = 0;
+							
+						}
+						cbYear.setSelectedIndex(year);
+					}
+				});
+				panel.add(btnNewButton_2);
+			}
+		}
+		{
+			panelCalendar = new JPanel();
+			contentPanel.add(panelCalendar, BorderLayout.CENTER);
+			panelCalendar.setLayout(new GridLayout(0, 7, 0, 0));
+		}
+		{
+			panel_1 = new JPanel();
+			contentPanel.add(panel_1, BorderLayout.SOUTH);
+			panel_1.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+			{
+				lblresult = new JLabel("생년월일");
+				lblresult.setHorizontalAlignment(SwingConstants.CENTER);
+				lblresult.setText(cbYear.getSelectedItem().toString() + cbMonth.getSelectedItem().toString() );
+				
+				panel_1.add(lblresult);
+			}
+			{
+				btnNewButton = new JButton("확인");
+				panel_1.add(btnNewButton);
+			}
+		}
+	}
+
+}
---
+++ src/WinMemberInsert.java
@@ -0,0 +1,272 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFileChooser;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import com.mysql.cj.protocol.Resultset;
+
+
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import javax.swing.JPasswordField;
+
+public class WinMemberInsert extends JDialog {
+
+	private final JPanel contentPanel = new JPanel();
+	private JTextField tfId;
+	private JTextField tfName;
+	private JTextField tfTel;
+	private JTextField tfBirth;
+	private String filePath = null;
+	private JPasswordField tfPw;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			WinMemberInsert dialog = new WinMemberInsert();
+			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			dialog.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public WinMemberInsert() {
+		setTitle("회원가입");
+		setBounds(100, 100, 364, 507);
+		getContentPane().setLayout(new BorderLayout());
+		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
+		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("ID");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel.setBounds(12, 26, 57, 15);
+		contentPanel.add(lblNewLabel);
+		
+		tfId = new JTextField();
+		tfId.setBounds(81, 23, 116, 21);
+		contentPanel.add(tfId);
+		tfId.setColumns(10);
+		
+		JLabel lblNewLabel_1 = new JLabel("PW");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel_1.setBounds(12, 74, 57, 15);
+		contentPanel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("이름");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel_2.setBounds(12, 122, 57, 15);
+		contentPanel.add(lblNewLabel_2);
+		
+		tfName = new JTextField();
+		tfName.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+					tfTel.requestFocus();
+				}
+			}
+		});
+		tfName.setColumns(10);
+		tfName.setBounds(81, 119, 116, 21);
+		contentPanel.add(tfName);
+		
+		JLabel lblNewLabel_3 = new JLabel("전화번호");
+		lblNewLabel_3.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel_3.setBounds(12, 170, 57, 15);
+		contentPanel.add(lblNewLabel_3);
+		
+		tfTel = new JTextField();
+		
+		
+		tfTel.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if(e.getKeyCode() == KeyEvent.VK_ENTER) {
+					tfBirth.requestFocus();
+				}
+			}
+		});
+		tfTel.setColumns(10);
+		tfTel.setBounds(81, 167, 116, 21);
+		contentPanel.add(tfTel);
+		
+		JLabel lblNewLabel_4 = new JLabel("생년월일");
+		lblNewLabel_4.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel_4.setBounds(12, 218, 57, 15);
+		contentPanel.add(lblNewLabel_4);
+		
+		tfBirth = new JTextField();
+		tfBirth.setColumns(10);
+		tfBirth.setBounds(81, 215, 116, 21);
+		contentPanel.add(tfBirth);
+		
+		JLabel lblNewLabel_5 = new JLabel("사진");
+		lblNewLabel_5.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblNewLabel_5.setBounds(12, 266, 57, 15);
+		contentPanel.add(lblNewLabel_5);
+		
+		JLabel lblpic = new JLabel("");
+		lblpic.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(e.getClickCount() == 2) {
+					JFileChooser chooser = new JFileChooser();
+					FileNameExtensionFilter filter = new FileNameExtensionFilter("이미지 파일", "jpg","png","gif");
+					chooser.addChoosableFileFilter(filter);
+					int ret = chooser.showOpenDialog(null);
+					
+					if(ret == JFileChooser.APPROVE_OPTION) {
+						filePath = chooser.getSelectedFile().getPath();
+						filePath = filePath.replaceAll("\\\\", "/");
+						ImageIcon image = new ImageIcon(filePath);
+						Image img = image.getImage();
+						img = img.getScaledInstance(120, 150, Image.SCALE_SMOOTH);
+						ImageIcon pic = new ImageIcon(img);
+						lblpic.setIcon(pic);
+						
+					}
+				}
+				
+			}
+		});
+		lblpic.setToolTipText("더블클릭 후 선택");
+		lblpic.setBackground(new Color(255, 255, 255));
+		lblpic.setOpaque(true);
+		lblpic.setBounds(81, 269, 120, 150);
+		
+		contentPanel.add(lblpic);
+		
+		JButton btnNewButton = new JButton("중복확인");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					ResultSet rs = null;
+					PreparedStatement pstmt = null;
+					String id = tfId.getText();
+					
+					Class.forName("com.mysql.cj.jdbc.Driver");
+					Connection conn = DriverManager.getConnection
+							("jdbc:mysql://localhost:3306/sqldb", "root","1234");
+					
+					String sql = "SELECT id FROM membertbl WHERE id=?";
+					pstmt = conn.prepareStatement(sql);
+					pstmt.setString(1, id);
+					rs = pstmt.executeQuery();
+					
+					if(rs.next()) {
+						String dbid = rs.getString("id");
+						if(dbid.equals(id));{
+							JOptionPane.showMessageDialog(null, "중복된 아이디!");
+							tfId.requestFocus();
+							tfId.setSelectionStart(0);
+							tfId.setSelectionEnd(tfId.getText().length()); //아이디 블럭치기
+							}
+						
+					}else {
+						JOptionPane.showMessageDialog(null, "사용가능한 아이디!");
+						tfPw.requestFocus();
+					}
+					
+					
+					} catch (Exception e1) {
+					e1.printStackTrace();
+					}
+				
+			}
+		});
+		btnNewButton.setBounds(212, 23, 95, 22);
+		contentPanel.add(btnNewButton);
+		
+		JButton btnCalendar = new JButton("Calendar..");
+		btnCalendar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				WinCalendar dialog = new WinCalendar();
+				dialog.setVisible(true);
+				
+			}
+		});
+		btnCalendar.setBounds(212, 215, 95, 22);
+		contentPanel.add(btnCalendar);
+		
+		tfPw = new JPasswordField();
+		tfPw.setBounds(81, 71, 116, 21);
+		contentPanel.add(tfPw);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
+			getContentPane().add(buttonPane, BorderLayout.SOUTH);
+			{
+				JButton btnJoin = new JButton("회원가입");
+				btnJoin.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						try {
+							Class.forName("com.mysql.cj.jdbc.Driver");
+							Connection conn = DriverManager.getConnection
+									("jdbc:mysql://localhost:3306/sqldb", "root","1234");
+							
+							String id = tfId.getText();
+							String pw = tfPw.getText();
+							String name = tfName.getText();
+							String tel = tfTel.getText().replaceAll("-", "");
+							String birth = tfBirth.getText();
+							
+							
+							
+							Statement stmt = conn.createStatement();
+							String sql = "INSERT INTO membertbl(id,pw,name,tel,birth,pic)"
+									+ " VALUES('"+id+"','"+pw+"','"+name+"','"+tel+"','"+birth+"','"+filePath+"')";
+							
+							
+							if(stmt.executeUpdate(sql) > 0) {
+								JOptionPane.showMessageDialog(null, "회원가입 성공!");
+							}
+							else {
+								JOptionPane.showMessageDialog(null, "회원가입 실패!");
+							}
+							
+							
+							} catch (Exception e1) {
+							e1.printStackTrace();
+							}
+					}
+				});
+				btnJoin.setActionCommand("OK");
+				buttonPane.add(btnJoin);
+				getRootPane().setDefaultButton(btnJoin);
+			}
+			{
+				JButton cancelButton = new JButton("취소");
+				cancelButton.setActionCommand("Cancel");
+				buttonPane.add(cancelButton);
+			}
+		}
+	}
+}
